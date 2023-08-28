c29345c0f74626723e5db4275e5a9c51a3c6ee4f
sofekung0726 <sofekung@gmail.com>
2023-08-28 16:20:15 +0700

    first commmit

---
+++ src/CommercialPlan.java
@@ -0,0 +1,9 @@
+
+public class CommercialPlan extends Plan {
+
+	@Override
+	void getRate() {
+		rate=7.50;
+	}
+
+}
---
+++ src/InstitutionalPlan.java
@@ -0,0 +1,9 @@
+
+public class InstitutionalPlan extends Plan {
+
+	@Override
+	void getRate() {
+		rate=5.50;
+	}
+
+}
